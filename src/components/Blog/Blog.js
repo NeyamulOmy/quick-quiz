40c9d0d875dc6615831e4ad56be976dbc163f9ba
NeyamulOmy <neyamul.omy@gmail.com>
2022-10-12 19:20:42 +0600

    added blog

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -1,9 +1,20 @@
 import React from 'react';
-
+import './Blog.css'
 const Blog = () => {
     return (
-        <div>
-            This is blog.
+        <div className='blogs'>
+            <div className='blog'>
+                <h3>What is the purpose of React Router"?</h3>
+                <p>ReactJS Router is mainly used for developing Single Page Web Applications. React Router is used to define multiple routes in the application. When a user types a specific URL into the browser, and if this URL path matches any 'route' inside the router file, the user will be redirected to that particular route.</p>
+            </div>
+            <div className='blog'>
+                <h3>How does Context API work?</h3>
+                <p>React.createContext() is all you need. It returns a consumer and a provider. Provider is a component that as it's names suggests provides the state to its children. It will hold the "store" and be the parent of all the components that might need that store. Consumer as it so happens is a component that consumes and uses the state.</p>
+            </div>
+            <div className='blog'>
+                <h3>What is the purpose of useRef() hook?</h3>
+                <p>The useRef hook returns a mutable ref object. This object has a property called .current. The value is persisted in the refContainer.current property. These values are accessed from the current property of the returned object. The .current property could be initialised to the passed argument initialValue e.g. useRef(initialValue). The object can persist a value for a full lifetime of the component. </p>
+            </div>
         </div>
     );
 };
