d723febe0d0b010632d5f268791d51f400eabdbf
NeyamulOmy <neyamul.omy@gmail.com>
2022-10-12 13:55:53 +0600

    added 404 path

--- src/App.js
+++ src/App.js
@@ -24,6 +24,10 @@ function App() {
         {
           path: 'topic',
           element: <TopicDetails></TopicDetails>
+        },
+        {
+          path: '*',
+          element: <h1> Error 404! Not Found</h1>
         }
       ]
 
