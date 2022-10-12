b438f759591a2eae5ac9293cdfb1d26b65770cff
NeyamulOmy <neyamul.omy@gmail.com>
2022-10-12 18:54:51 +0600

    added heroicons to show correct answer

--- src/components/Questions/Questions.js
+++ src/components/Questions/Questions.js
@@ -2,6 +2,11 @@ import React from 'react';
 import './Questions.css';
 import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEye } from '@fortawesome/free-solid-svg-icons'
+import { Button } from 'react-bootstrap';
+
+
 const Questions = ({ question }) => {
     const notify = (option, ans) => {
         let msg = '';
@@ -13,7 +18,12 @@ const Questions = ({ question }) => {
         }
         toast(msg, {
             position: toast.POSITION.TOP_CENTER,
-            toastId: 'wrong'
+
+        })
+    }
+    const correctAns = (correctAns) => {
+        toast(`Correct answer is: ${correctAns}`, {
+            position: toast.POSITION.TOP_CENTER
         })
     }
     let qs = question.question;
@@ -27,6 +37,8 @@ const Questions = ({ question }) => {
 
             <h2>Q: {qs}</h2>
             <h4>Options:</h4>
+            <div className='right-align'><Button variant='light' onClick={() => { correctAns(question.correctAnswer) }}><FontAwesomeIcon icon={faEye} /></Button></div>
+
             <div className='left-align f20px grid'>
                 {options.map(option => <div><input onClick={() => { notify(option, question.correctAnswer) }} type="radio" id={option} name={question.id} value={option} />
                     <label for="html">{option}</label></div>)}
