fd043aeba716db7b35ae973e26df72f875fe888c
NeyamulOmy <neyamul.omy@gmail.com>
2022-10-12 20:01:26 +0600

    added barchart

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,13 +1,19 @@
 import './Statistics.css'
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
-
+import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts'
 const Statistics = () => {
     const stats = useLoaderData();
+
     return (
-        <div>
-            This is stats {stats.data.length}
-        </div>
+
+        <BarChart width={500} height={400} data={stats.data}>
+            <Bar dataKey="total" fill="#8884d8" />
+            <XAxis dataKey="name" />
+            <YAxis />
+        </BarChart>
+
+
     );
 };
 
